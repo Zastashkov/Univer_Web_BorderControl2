bb02d2a22040c5b4c0fcd5f1996625c2d536736a
Daniel Zastashkov <zast2020@mail.ru>
2022-12-02 21:44:37 +0300

    env fixed

--- config/database.php
+++ config/database.php
@@ -66,7 +66,7 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'http://zast.std-2047.ist.mospolytech.ru'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
